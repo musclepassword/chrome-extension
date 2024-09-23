11dada54a3881c74c42fc25f9c92ded498120a21
Cüneyd BÖLÜKOĞLU <cuneydbolukogluu@gmail.com>
2024-09-23 15:06:55 +0300

    update

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,10 +1,30 @@
+import { Space, Table, Tag } from 'antd';
 import { usePasswordStore } from '../../stores';
 
 const History = () => {
   const passwordState = usePasswordStore((state) => state.password)
 
+  const columns = [
+    {
+      title: 'Passwords',
+      // dataIndex: (key) => key,
+      key: (key) => key,
+      render: (text) => <a>{text}</a>,
+    },
+  ];
+  
+  const rowSelection = {
+    onChange: (e) => console.log(e),
+  };
+
   return (
-    <div>{passwordState.map(item => <div>{item}</div>)}</div>
+    // <div>{passwordState.map(item => <div>{item}</div>)}</div>
+    <Table
+      size='small'
+      rowSelection={rowSelection}
+      columns={columns}
+      dataSource={passwordState}
+    />
   )
 }
 
